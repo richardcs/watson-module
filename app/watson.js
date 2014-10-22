2fad738471a4f02a2cf60599ceb94fbdfc9b8d9d
Matt Greenberg <mgreenberg@gmail.com>
2014-10-22 16:14:41 -0700

    Cleaning up ugly whitespace

--- app/watson.js
+++ app/watson.js
@@ -4,10 +4,10 @@ module.exports = {
         console.log('query: ' + query);
         return '34 cats/(ironing board)^2';
     },
-    
+
     post: function (webhook, body, callback) {
         //console.log('POSTing '  +JSON.stringify(body) + ' to ' + webhook);
         request.post({url: webhook, body: body, json: true},  callback);
-    }   
+    }
 };
 
