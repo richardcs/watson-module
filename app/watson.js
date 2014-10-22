2d1b76b9aa0644c0fdd0d959c2d5b3c0dd0b361a
Richard Levenberg <richard.levenberg@connectsolutions.com>
2014-10-22 16:04:58 -0700

    working test and app for watson

---
+++ app/watson.js
@@ -0,0 +1,13 @@
+var request = require('request');
+module.exports = {
+    query: function (query) {
+        console.log('query: ' + query);
+        return '34 cats/(ironing board)^2';
+    },
+    
+    post: function (webhook, body, callback) {
+        //console.log('POSTing '  +JSON.stringify(body) + ' to ' + webhook);
+        request.post({url: webhook, body: body, json: true},  callback);
+    }   
+};
+
---
+++ test/watson.js
@@ -0,0 +1,36 @@
+var assert = require('chai').assert;
+var request = require('request');
+var express = require('express');
+var freeport = require('freeport');
+var watson = require('../app/watson');
+var app = express();
+
+app.post('/', function (req, res, next) {
+    console.log('request received', new Date());
+    res.status(200).end();
+});
+
+describe('A service that pulls { webhook: url, query: query } objects off a queue, makes the query and POSTs the respone to the webhook', function () {
+    before(function(done) {
+        freeport(function(err, port) {
+            this.uri = "http://localhost:" + port + "/";
+            this.server = app.listen(port, done);
+        }.bind(this));
+    });
+
+    it('should make a query to a webservice and return a response', function(done) {
+        var response = watson.query("What is the temperature in Gnome, Alaska");
+        console.log(response);
+        assert(response);
+        done();
+    });
+
+    it('should POST a response to a webhook', function(done) {
+        watson.post(this.uri, { message: 'a post body' }, function(err, res, body){
+            console.log("err", err);
+            console.log("res", res);
+            assert.equal(res.statusCode, 200)
+            done();
+        });
+    });
+});
