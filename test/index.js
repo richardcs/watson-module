7226c00878b49252e5aeb259e1355798a6ce6384
Matt Greenberg <mgreenberg@gmail.com>
2014-10-22 10:23:44 -0700

    Added a basic index and test for getting things running. A copy of what Sam did.

---
+++ test/index.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var request = require('request');
+
+describe('An HTTP Server', function () {
+
+  it('should make a request', function(done) {
+    request('http://localhost:3000/', function(err, resp, body) {
+      if (err) {
+        throw err;
+      }
+
+      assert.equal(resp.statusCode, 200);
+      done();
+    })
+  })
+});
