941f2aa2503208f093a0f56df15b3491a4416cbb
Steve Rozzo <steve.rozzo@connectsolutions.com>
2014-10-22 16:04:58 -0700

    implement job queue creation

--- app/webapp.js
+++ app/webapp.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var winston = require('winston');
+var kue = require('kue');
 
+var jobs = kue.createQueue(); // create jobs queue
 var app = express(); // setup the web server
 
 app.use( bodyParser.json() );                          // to support JSON-encoded bodies
@@ -10,9 +12,21 @@ app.use( bodyParser.urlencoded({ extended: false }) ); // to support URL-encoded
 app.post('/', function(req, res) {
     var query = req.body.query,
         webhook = req.body.webhook;
+
     if (query && webhook) {
       winston.info('you posted query: '+query+', and webhook:'+webhook);
       res.status(201).send('query ('+query+') is running and will respond to webhook ('+webhook+') if watson hears you');
+      var job = jobs.create('winston', {
+          title: query
+        , query: query
+        , webhook: webhook
+      }).save( function(err){
+         if( !err ) {
+           winston.info( job.id )
+         } else {
+           winston.error( 'Job for query: ' + query ' and webhook: ' + webhook ' failed: ' + err )
+         }
+      });
     } else {
       res.status(412).send('query and/or webhook parameters are missing');
     }
